70546607fb924ad7707adad19e4ec05797ca8196
Samantha Pardini <60455779+samantha-pardini@users.noreply.github.com>
2020-02-14 21:39:08 -0800

    Exercises 1,3 and 4
    Exercises one, three and four are finished

--- Practice_3.scala
+++ Practice_3.scala
@@ -1,1 +1,61 @@
 
+// 1st Algorithm 
+//Recursive version descending
+def algorithm1(n: Int): Int = 
+{   
+    if (n<2)
+    {
+        return n
+    }
+    else
+    {
+        return algorithm1 (n-1) + algorithm1(n-2)
+    }
+}
+println(algorithm1(1))
+
+
+// 2nd Algorithm 
+//Version with explicit formula 
+
+
+// 3rd Algorithm 
+//Iterative version
+def algorithm3 (n: Int): Int =
+{
+var a = 0
+var b = 1
+var c = 0
+    for (k <- 1 to n)
+    {   
+        println(a)
+        c = b + a
+        a = b
+        b = c
+    }
+    return(a)
+}
+println(algorithm3(3))
+
+
+// 4th Algorithm 
+//iterative version-2 variables
+def algorithm4(n: Int): Int =
+{
+    var a = 0
+    var b = 1
+    for(k <- 1 to n)
+        {
+            b = b + a
+            a = b - a
+        }
+        return(a)
+}
+println(algorithm4(3))
+
+// 5th Algorithm
+//Iterative vector version (Complexity)
+
+
+//6th Algorithm 
+//Version Divide and Conquer
