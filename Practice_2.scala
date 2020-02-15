2a83ba1936d76f1754310c49e42233f198a033fc
Samantha Pardini <60455779+samantha-pardini@users.noreply.github.com>
2020-02-14 22:07:51 -0800

    Update- Exercises 1-4
    Exercises 1-4

--- Practice_2.scala
+++ Practice_2.scala
@@ -1,1 +1,12 @@
+// 1. Create a list called "list" with the elements "red", "white", "black"
+val list = List ("red", "white", "black")
+println (list)
+
+// 2. Add 5 more items to "list" "green", "yellow", "blue", "orange", "pearl"
+val list1 = "green" :: "yellow" :: "blue" :: "orange" :: "pearl" :: list
+
+// 3. Bring the "list" "green", "yellow", "blue" items
+list.slice (0,3)
+// 4. Create a number array in the 1-1000 range in 5-in-5 steps
+Array.range (1,1000,5)
 
