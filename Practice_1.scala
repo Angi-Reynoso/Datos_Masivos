a769d2cf507ef32f2c5d1de67bb63b2334d2c06c
Angélica Reynoso <52439492+Angi-Reynoso@users.noreply.github.com>
2020-02-15 03:59:48 -0800

    Update Practice_1.scala
    Language correction in variables. 
    Exercises 1 to 4.

--- Practice_1.scala
+++ Practice_1.scala
@@ -1,52 +1,52 @@
-// Assessment 1
+// Practice 1
 //1. Develop an algorithm in scala that calculates the radius of a circle
 println("1. Radius of a circle")
-var diametro: Double = 15
-var radio: Double = (diametro/2)
-println("Operacion = " + {diametro} + "/" + "2" + " = " + radio)
+var D: Double = 15
+var R: Double = (D/2)
+println("Operation = " + {D} + "/" + "2" + " = " + R)
 println("")
 
 //2. Develop an algorithm in scala that tells me if a number is prime
     // Prime numbers: 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 
     // 53, 59, 61, 67, 71, 73, 79, 83, 89 y 97.
 println("2. Prime numbers")
-var numero: Double = 1
-if(numero == 2 || numero == 3 || numero == 5 || numero == 7){
-           var mensaje = s"${numero} es un numero primo"
-           println(mensaje)
-        } else if(numero % 2 == 0){
-           var mensaje = s"${numero} no es un numero primo"
-           println(mensaje)
-        } else if(numero % 3 == 0){
-           var mensaje = s"${numero} no es un numero primo"
-           println(mensaje)
-        } else if(numero % 5 == 0){
-           var mensaje = s"${numero} no es un numero primo"
-           println(mensaje)
-        } else if(numero % 7 == 0){
-           var mensaje = s"${numero} no es un numero primo"
-           println(mensaje)
-        } else if(numero == 1){
-           var mensaje = s"${numero} no es un numero primo"
-           println(mensaje)
+var num: Double = 1
+if(num == 2 || num == 3 || num == 5 || num == 7){
+           var message = s"${num} it's a prime number"
+           println(message)
+        } else if(num % 2 == 0){
+           var message = s"${num} it's not a prime number"
+           println(message)
+        } else if(num % 3 == 0){
+           var message = s"${num} it's not a prime number"
+           println(message)
+        } else if(num % 5 == 0){
+           var message = s"${num} it's not a prime number"
+           println(message)
+        } else if(num % 7 == 0){
+           var message = s"${num} it's not a prime number"
+           println(message)
+        } else if(num == 1){
+           var message = s"${num} it's not a prime number"
+           println(message)
         } else {
-           var mensaje = s"${numero} es un numero primo"
-           println(mensaje)
+           var message = s"${num} it's a prime number"
+           println(message)
         }
 println("")
       
 //3. Given the variable bird = "tweet", use string interpolation to
-//   print "Estoy ecribiendo un tweet"
+//   print "I am writing a tweet"
 println("3. String interpolation")
 val bird = "tweet"
-val imprimir = s"Estoy escribiendo un ${bird}"
-println(imprimir)
+val res = s"I am writing a ${bird}"
+println(res)
 println("")
 
-//4. Given the variable message = "Hola Luke yo soy tu padre" use slilce to extract the
+//4. Given the variable message = "Hi Luke, I'm your father" use slice to extract the
 //sequence "Luke"
-val message = "Hola Luke yo soy tu padre"
-message slice (5,9)
+val message = "Hi Luke, I'm your father"
+message slice (3,7)
 
 //5. What is the difference between value and a variable in scala?
 // Value: Used to assign an immutable value (which cannot be changed, static)
