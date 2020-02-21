6342743dd8891d9ad513cc15dfc5a6aeceadc9fc
Samantha Pardini <60455779+samantha-pardini@users.noreply.github.com>
2020-02-20 21:12:24 -0800

    Practice 4.scala
    Solutions 
    1-10

---
+++ Practice_4.scala
@@ -0,0 +1,20 @@
+
+1.  `df.select(avg($"Close")).show()`
+
+2.  `df.select(collect_list("Close")).show()` 
+
+3.  `df.select(collect_set("Close")).show()`
+
+4.  `df.select(first("Close")).show()`
+
+5.  `df.select(last("Close")).show()`
+
+6.  `df.select(max("Close")).show()`
+
+7.  `df.select(min("Close")).show()`
+
+8.  `df.select(skewness("Low")).show()`
+
+9. `df.select(sum("High")).show()`
+
+10.  `df.select(kurtosis("Low")).show()`
