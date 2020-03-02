62853cd3bd04df1a6d1ada9649eed0c0d675c1bb
Samantha Pardini <60455779+samantha-pardini@users.noreply.github.com>
2020-03-01 19:53:46 -0800

    Update Exam_1b.scala
    Solution: 9-11e

--- Exams/Exam_1b/Exam_1b.scala
+++ Exams/Exam_1b/Exam_1b.scala
@@ -32,3 +32,40 @@ df2.show()
 //8. What day had the highest peak in the Close column?
 df.orderBy($"Close".desc).show(1)
 df.select(max("Close")).show()
+
+//9. Meaning of the Close column
+df.describe("Close").show()
+//The Close column refers to closing prices, that is, the last level at which an asset was 
+//traded before the market closed on a given day.
+//La columna Close se refiere a los precios de cierre, es decir, el último nivel en el que 
+//se negoció un activo antes de que el mercado cerrara en un día determinado.
+
+//10. Maximum and minimum of the Volume column
+df.select(max("Volume")).show()
+df.select(min("Volume")).show()
+
+//11.
+//11a. How many days was the Close column less than 600?
+val res2 = df.filter($"Close"<600)
+res2.select(count("Close")).show()
+
+//11b. What percentage of the time was the High column greater than 500?
+(df.filter($"High">500).count()*1.0 / df.count()) * 100
+
+//df.filter($"High">500).count() = registros mayores a 500
+//df.count() = total de registros
+
+//11c. What is Pearson's correlation between the High column and the Volume column?
+df.select(corr("High", "Volume")).show() 
+
+//11d. What is the maximum of the High column per year?
+val df2 = df.withColumn("Year", year(df("Date")))
+val dfmax = df2.groupBy("Year").max()
+val res3 = dfmax.select($"Year", $"max(High)")
+res3.orderBy($"Year".asc).show()
+
+//11e. What is the average of the Close column for each calendar month?
+val df3 = df.withColumn("Month", month(df("Date")))
+val dfavg = df3.groupBy("Month").mean()
+val res4 = dfavg.select($"Month", $"avg(Close)")
+res4.orderBy($"Month".asc).show()
