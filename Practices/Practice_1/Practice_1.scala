c70bc6ae80cdc9a5e898700e0c34ba1e2fc5a2d5
Samantha Pardini <60455779+samantha-pardini@users.noreply.github.com>
2020-05-04 11:56:43 -0700

    Update Practice_1.scala

--- Practices/Practice_1/Practice_1.scala
+++ Practices/Practice_1/Practice_1.scala
@@ -1,5 +1,4 @@
 // Import LinearRegression
-import org.apache.spark.sql.SparkSession
 import org.apache.spark.ml.regression.LinearRegression
 
 // Optional: use the following code to configure errors
@@ -7,7 +6,7 @@ import org.apache.log4j._
 Logger.getLogger("org").setLevel(Level.ERROR)
 
 //Start a simple Spark Session
-
+import org.apache.spark.sql.SparkSession
 val spark = SparkSession.builder().getOrCreate()
 
 //Use Spark for the Clean-Ecommerce csv file.
@@ -18,22 +17,26 @@ data.printSchema
 
 //Print an example row from the DataFrane.
 data.head(1)
+val colnames = data.columns
+val firstrow = data.head(1)(0)
+println("\n")
+println("Example data row")
+for(ind <- Range(1, colnames.length)){
+    println(colnames(ind))
+    println(firstrow(ind))
+    println("\n")
+}
 
 //Transform the data frame so that it takes the form of
 // ("label", "features")
-
-
 // Import VectorAssembler and Vectors
 import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.linalg.Vectors
 
 // Rename the Yearly Amount Spent column as "label"
 // Also from the data take only the numerical column
-// Leave all this as a new DataFrame called df
+// set all this as a new DataFrame called df
 val df = data.select(data("Yearly Amount Spent").as("label"),$"Avg Session Length",$"Time on App",$"Time on Website",$"Length of Membership")
-
-
-// Have the assembler object convert the input values to a vector
 
 
 // Use the VectorAssembler object to convert the input columns of the df
@@ -53,7 +56,7 @@ val p1Model = p1.fit(output) //ajustar el output
 
 
 // Print the coefficients and intercept for the linear regression
-
+println(s"Coefficients: ${p1Model.coefficients} Intercept: ${p1Model.intercept}")
 
 // Summarize the model on the training set print the output of some metrics!
 // Use our model's .summary method to create an object
