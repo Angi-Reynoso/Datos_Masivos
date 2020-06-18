f042590110fc667c2bd39baec85febc40f9a1937
Samantha Pardini <60455779+samantha-pardini@users.noreply.github.com>
2020-06-17 18:29:35 -0700

    Update MultilayerPerceptron.scala

--- Unit_4/MultilayerPerceptron.scala
+++ Unit_4/MultilayerPerceptron.scala
@@ -1,1 +1,64 @@
-
+import org.apache.spark.sql.SparkSession
+val spar = SparkSession.builder().getOrCreate()
+ 
+import org.apache.log4j._
+Logger.getLogger("org").setLevel(Level.ERROR)
+ 
+val df = spark.read.option("header", "true").option("inferSchema","true")csv("bank-additional-full.csv")
+ 
+import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+ 
+val data = df.select(df("y").as("label"), $"age",$"duration",$"campaign",$"pdays",$"previous",$"emp_var_rate",$"cons_price_idx",$"cons_conf_idx",$"euribor3m",$"nr_employed")
+ 
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.linalg.Vectors
+ 
+val assembler = new VectorAssembler().setInputCols(Array("age","duration","campaign","pdays","previous","emp_var_rate","cons_price_idx","cons_conf_idx","euribor3m","nr_employed")).setOutputCol("features")
+ 
+val features = assembler.transform(data)
+ 
+import org.apache.spark.ml.feature.StringIndexer
+val labelIndexer = new StringIndexer().setInputCol("label").setOutputCol("indexedLabel").fit(features)
+ 
+import org.apache.spark.ml.feature.VectorIndexer
+val featureIndexer = new VectorIndexer().setInputCol("features").setOutputCol("indexedFeatures").setMaxCategories(4).fit(features)
+ 
+val splits = features.randomSplit(Array(0.7, 0.3), seed = 1234L)
+val train = splits(0)
+val test = splits(1)
+ 
+val t1 = System.nanoTime
+ 
+val layers = Array[Int](10, 11, 3, 2)
+ 
+val trainer = new MultilayerPerceptronClassifier()
+.setLayers(layers)
+.setLabelCol("indexedLabel")
+.setFeaturesCol("indexedFeatures")
+.setBlockSize(128)
+.setSeed(1234L)
+.setMaxIter(100)
+ 
+import org.apache.spark.ml.feature.IndexToString
+val labelConverter = new IndexToString()
+.setInputCol("prediction")
+.setOutputCol("predictedLabel")
+.setLabels(labelIndexer.labels)
+ 
+import org.apache.spark.ml.Pipeline
+val pipeline = new Pipeline()
+.setStages(Array(labelIndexer, featureIndexer, trainer, labelConverter))
+val model = pipeline.fit(train)
+val predictions = model.transform(test)
+val predictionAndLabels = predictions.select("prediction", "label")
+ 
+val evaluator = new MulticlassClassificationEvaluator()
+.setLabelCol("indexedLabel")
+.setPredictionCol("prediction")
+.setMetricName("accuracy")
+ 
+val accuracy = evaluator.evaluate(predictions)
+println("Test Error = " + (1.0 - accuracy))
+ 
+val duration = (System.nanoTime - t1) / 1e9d
